4de967fcbcba46da71f01063a3bc79866dd98e5a
Teerasej Jiraphatchandej <teerasej@gmail.com>
2022-03-13 16:00:34 +0700

    implement express api in typescript

--- app.ts
+++ app.ts
@@ -1,2 +1,11 @@
-const username:string = 'Nextflow';
-console.log(`Hello ${username}`);
+import express, { Request, Response } from 'express'    
+
+const app = express()
+
+app.get('/', (req:Request, res:Response) => {
+    res.send('Hello World!')
+})
+
+app.listen(3000, () => {
+    console.log('Example app listening on port http://localhost:3000')
+})
